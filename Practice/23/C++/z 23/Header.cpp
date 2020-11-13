fce667811c5d73b076d4cbe5bdfa3e2fcfcdb1d8
Your Name <s.grigorkevich21@mail.ru>
2020-11-13 11:24:30 +0300

    23zcom

---
+++ Practice/23/C++/z 23/Header.cpp
@@ -0,0 +1,18 @@
+#include "Header.h"
+
+int otw(int a)
+{
+    long long b = 1;
+    if (a > 1) {
+        a = a - 1;
+        b = a * b;
+    }
+
+    return b;
+    b = 1;
+}
+
+int otw(int a, std::cin >> a)
+{
+    return 0;
+}
---
+++ Practice/23/C++/z 23/Header.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <iostream>
+
+#pragma once
+int otw(int a);
+
